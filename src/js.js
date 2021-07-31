10f0f08e8a66ffea677406982ae8ea4f742835a2
Gina Sala <georgina.sala18@gmail.com>
2021-07-31 15:41:41 -0500

    about.html HTML and CSS

--- src/js.js
+++ src/js.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector('nav');
 window.addEventListener('scroll', function(){
-    if(window.pageYOffset > 500){
+    if(window.pageYOffset > 50){
         nav.classList.add('bg-dark', 'shadow');
     } else {
         nav.classList.remove('bg-dark', 'shadow');
