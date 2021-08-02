10c13a215e02bb3b773f4cc37ab6e7c7ae5f4998
Gina Sala <georgina.sala18@gmail.com>
2021-08-01 20:14:31 -0500

    Changed color palette

--- src/js.js
+++ src/js.js
@@ -1,8 +1,34 @@
 let nav = document.querySelector('nav');
+
 window.addEventListener('scroll', function(){
+    let navBackground = document.querySelector(".navbar");
     if(window.pageYOffset > 50){
-        nav.classList.add('bg-dark', 'shadow');
+        navBackground.style.backgroundColor = "black";
     } else {
-        nav.classList.remove('bg-dark', 'shadow');
+        navBackground.style.backgroundColor = "transparent";
     }
 })
+
+function setNewWeatherImage() {
+    document.getElementById("weather-image").src = "images/weather-app.PNG";
+}
+
+function setOldWeatherImage() {
+    document.getElementById("weather-image").src = "images/weather-app- b&n.PNG";
+}
+
+function setNewMoviesImage() {
+    document.getElementById("movies-image").src = "images/movies-app.PNG";
+}
+
+function setOldMoviesImage() {
+    document.getElementById("movies-image").src = "images/movies-app - b&n.PNG";
+}
+
+function setNewCalculatorImage() {
+    document.getElementById("calculator-image").src = "images/calculator-app.PNG";
+}
+
+function setOldCalculatorImage() {
+    document.getElementById("calculator-image").src = "images/calculator-app- b&n.PNG";
+}
